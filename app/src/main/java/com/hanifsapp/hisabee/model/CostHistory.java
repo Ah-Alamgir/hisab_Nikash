e1e3fe797d48d82b7d0ac3eae115ae63ccb79c7c
Ah-Alamgir <ahAlamgir16@outlook.com>
2024-02-17 19:46:20 +0600

    hanif

---
+++ app/src/main/java/com/hanifsapp/hisabee/model/CostHistory.java
@@ -0,0 +1,38 @@
+package com.hanifsapp.hisabee.model;
+
+public class CostHistory {
+    int amount, type;
+    String date;
+    public CostHistory() {
+    }
+
+    public CostHistory(int amount, int type, String date) {
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
